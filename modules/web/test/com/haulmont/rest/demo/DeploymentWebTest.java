bf87f0bbf00d23f6b5e923c6f6608f02d190dbd9
Nikita Shchienko <shchienko@haulmont.com>
2019-06-11 11:34:36 +0400

    Set up deployment tests #1 (#5)
    Set up deployment tests #1

--- modules/web/test/com/haulmont/rest/demo/DeploymentWebTest.java
+++ modules/web/test/com/haulmont/rest/demo/DeploymentWebTest.java
@@ -1,6 +1,7 @@
 package com.haulmont.rest.demo;
 
 import com.haulmont.rest.demo.core.app.PortalTestService;
+import org.junit.Before;
 import org.junit.Test;
 
 import static com.haulmont.rest.demo.RestUtils.*;
@@ -10,27 +11,33 @@ public class DeploymentWebTest {
 
     protected static final String URI_BASE_WEB = "http://localhost:8080/app/rest/v2";
 
+    protected String oauthTokenWeb;
+
+    @Before
+    public void beforeEach() throws Exception {
+        oauthTokenWeb = getAuthToken(URI_BASE_WEB);
+    }
+
     @Test
-    public void getTokenWeb() throws Exception {
-        String oauthTokenWeb = getAuthToken(URI_BASE_WEB);
+    public void getTokenWeb() {
         assertNotNull(oauthTokenWeb);
     }
 
     @Test
     public void loadSomeListWeb() throws Exception {
         String url = URI_BASE_WEB + "/entities/sec$User";
-        loadSomeList(url, getAuthToken(URI_BASE_WEB));
+        loadSomeList(url, oauthTokenWeb);
     }
 
     @Test
     public void executeQueryWeb() throws Exception {
         String url = URI_BASE_WEB + "/queries/sec$User/currentUser";
-        executeQuery(url, getAuthToken(URI_BASE_WEB));
+        executeQuery(url, oauthTokenWeb);
     }
 
     @Test
     public void executeServiceWeb() throws Exception {
         String url = URI_BASE_WEB + "/services/" + PortalTestService.NAME + "/sum";
-        executeService(url, getAuthToken(URI_BASE_WEB));
+        executeService(url, oauthTokenWeb);
     }
 }
